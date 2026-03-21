639098ae6f46ca4322fdef32a88058719c491b84
HSandanima <himandi.20234070@iit.ac.lk>
2026-03-21 12:33:50 +0530

    add tabbed interface for reservations and waiter allocations

--- app/menu/foh/reservations/page.tsx
+++ app/menu/foh/reservations/page.tsx
@@ -2,12 +2,15 @@
 
 import AddReservationModal from "@/components/foh/AddReservationModal";
 import ReservationHistoryTable from "@/components/foh/ReservationHistoryTable";
+import WaiterAllocations from "@/components/foh/WaiterAllocations";
 import dynamic from "next/dynamic";
 import { useState } from "react";
+import { TabPanel, TabView } from "primereact/tabview";
 
 const Button = dynamic(() => import("@/components/Button"), { ssr: false });
 
 export default function ReservationsPage() {
+  const [activeIndex, setActiveIndex] = useState(0);
   const [modalVisible, setModalVisible] = useState(false);
   const [reservationRefreshKey, setReservationRefreshKey] = useState(0);
 
@@ -30,18 +33,38 @@ export default function ReservationsPage() {
           <h1 className="text-5xl font-bold text-[#FF6B6B]">
             Customer Reservations
           </h1>
-          <Button
-            id="addReservationButton"
-            text="Add Reservation"
-            type="button"
-            className="bg-[#FF6B6B] text-white px-4 py-2 rounded-xl shadow-md"
-            state={true}
-            onClick={handleAddReservation}
-          />
+          {activeIndex === 0 ? (
+            <Button
+              id="addReservationButton"
+              text="Add Reservation"
+              type="button"
+              className="bg-[#FF6B6B] text-white px-4 py-2 rounded-xl shadow-md"
+              state={true}
+              onClick={handleAddReservation}
+            />
+          ) : null}
         </div>
 
         <div className="overflow-hidden">
-          <ReservationHistoryTable refreshTrigger={reservationRefreshKey} />
+          <TabView
+            activeIndex={activeIndex}
+            onTabChange={(e) => setActiveIndex(e.index)}
+            className="custom-tabs"
+          >
+            <TabPanel header="Reservation History">
+              <div className="pt-8">
+                <ReservationHistoryTable
+                  refreshTrigger={reservationRefreshKey}
+                />
+              </div>
+            </TabPanel>
+
+            <TabPanel header="Allocate waiters">
+              <div className="pt-2">
+                <WaiterAllocations />
+              </div>
+            </TabPanel>
+          </TabView>
         </div>
       </div>
 
